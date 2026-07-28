8a1b8d2b282cb7c2ae29666dcc5f35caf7d82364
honeok <i@honeok.com>
2026-07-29 00:54:20 +0800

    refactor: simplify image version retrieval functions
    Consolidate image version retrieval logic into a single function
    that handles both latest and current versions. This improves code
    readability and maintainability by reducing redundancy. Update
    function names for clarity and consistency.
    
    Signed-off-by: honeok <i@honeok.com>

--- images/kubernetes-core-images.sh
+++ images/kubernetes-core-images.sh
@@ -12,58 +12,45 @@ CORE_IMAGES_FILE="./kubernetes-core-images.md"
 : "${ALIYUN_REGISTRY:?missing ALIYUN_REGISTRY}"
 : "${ALIYUN_NAMESPACE:?missing ALIYUN_NAMESPACE}"
 
-_die() {
+die() {
     printf '[%s] %s\n' "$(date '+%F %T')" "[ERROR] $*"
     exit 1
 }
 
-_log() {
+log() {
     printf '[%s] %s\n' "$(date '+%F %T')" "[INFO] $*"
 }
 
 ## functions library
-get_latest_ver() {
-    local img regex
+get_img_ver() {
+    local img mode
 
     img="$1"
-    regex="${2:-}"
+    mode="$2"
 
-    if [ -z "$regex" ]; then
-        case "$img" in
-        *pause)
-            regex='^[0-9]+(\.[0-9]+){1,2}$'
-            ;;
-        *kube-apiserver | *kube-controller-manager | *kube-scheduler | *kube-proxy | *coredns)
-            regex='^v[0-9]+(\.[0-9]+){2}$'
-            ;;
-        *etcd)
-            regex='^[0-9]+(\.[0-9]+){2}-[0-9]+$'
-            ;;
-        esac
-    fi
-    skopeo list-tags "docker://$img" | jq -r '.Tags[]?' | grep -E -- "$regex" | sort -V | tail -n 1
-}
+    case "$img" in
+    *pause)
+        regex='^[0-9]+(\.[0-9]+){1,2}$'
+        ;;
+    *kube-apiserver | *kube-controller-manager | *kube-scheduler | *kube-proxy | *coredns)
+        regex='^v[0-9]+(\.[0-9]+){2}$'
+        ;;
+    *etcd)
+        regex='^[0-9]+(\.[0-9]+){2}-[0-9]+$'
+        ;;
+    esac
 
-get_current_ver() {
-    local img regex
-
-    img="$1"
-    regex="${2:-}"
-
-    if [ -z "$regex" ]; then
-        case "$img" in
-        *pause)
-            regex='^[0-9]+(\.[0-9]+){1,2}$'
-            ;;
-        *kube-apiserver | *kube-controller-manager | *kube-scheduler | *kube-proxy | *coredns)
-            regex='^v[0-9]+(\.[0-9]+){2}$'
-            ;;
-        *etcd)
-            regex='^[0-9]+(\.[0-9]+){2}-[0-9]+$'
-            ;;
-        esac
-    fi
-    awk -F: -v img="$img" '$1 == img { print $2; exit }' "$CORE_IMAGES_FILE" | grep -E -- "$regex"
+    case "$mode" in
+    latest)
+        skopeo list-tags "docker://$img" | jq -r '.Tags[]?' | grep -E -- "$regex" | sort -V | tail -n 1
+        ;;
+    current)
+        awk -F: -v img="$img" '$1 == img { print $2; exit }' "$CORE_IMAGES_FILE" | grep -E -- "$regex"
+        ;;
+    *)
+        die "invalid mode: $mode"
+        ;;
+    esac
 }
 
 ver_gt() {
@@ -72,7 +59,7 @@ ver_gt() {
     [[ "$l" != "$c" && "$(printf '%s\n' "$l" "$c" | sort -V | tail -n 1)" == "$l" ]]
 }
 
-update_img_ver() {
+update_and_edit() {
     local img="$1" ver="$2"
 
     sed -Ei "s#^($(printf '%s\n' "$img" | sed 's#[][(){}.^$*+?|/\\]#\\&#g')):[^[:space:]]+\$#\1:${ver}#" "$CORE_IMAGES_FILE"
@@ -85,7 +72,7 @@ sync_img() {
     local dst
 
     dst="$ALIYUN_REGISTRY/$ALIYUN_NAMESPACE/${img##*/}:$tag"
-    docker buildx imagetools inspect "$dst" > /dev/null 2>&1 && _log "$dst already exists, skip." && return
+    docker buildx imagetools inspect "$dst" > /dev/null 2>&1 && log "$dst already exists, skip." && return
     docker pull "$img:$tag"
     docker tag "$img:$tag" "$dst"
     docker push "$dst"
@@ -106,10 +93,10 @@ KUBERNETES_CORE_IMAGES=(
 # docker login "$ALIYUN_REGISTRY" -u "$ALIYUN_USERNAME" --password-stdin <<< "$ALIYUN_PASSWORD" 2> /dev/null
 
 for i in "${KUBERNETES_CORE_IMAGES[@]}"; do
-    latest_ver="$(get_latest_ver "$i")"
-    current_ver="$(get_current_ver "$i")"
+    latest_ver="$(get_img_ver "$i" latest)"
+    current_ver="$(get_img_ver "$i" current)"
     ver_gt "$latest_ver" "$current_ver" || continue
-    _log "$i update: $current_ver -> $latest_ver"
-    update_img_ver "$i" "$latest_ver"
+    log "$i update: $current_ver -> $latest_ver"
+    update_and_edit "$i" "$latest_ver"
     sync_img "$i" "$latest_ver"
 done
